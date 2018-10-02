2b4237a6922fd628f158afb860aae353d1ce8899
alwinmreji <alwinreji18@gmail.com>
2018-10-02 11:48:01 +0530

    *.c to *.ino

--- main0.ino
+++ main0.ino
@@ -2,23 +2,23 @@
 void ictester();
 void ictrainer();
 using namespace std;
-void setup() {
-  Serial.begin(9600);  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(9600);
 }
-int choice;
+char choice;
 void loop()
 {
-
-printf("1-IC Trainer \n 2-IC Tester \n Enter choice: \n");
-scanf("%d",&choice);
-switch(choice)
-{
-  case 1: ictrainer();
-          break;
-  case 2: ictester();
-          break;
-  default: ictrainer();
-};
+  Searial.print("1-IC Trainer \n 2-IC Tester \n\t Enter choice: \n");
+  choice = Searial.read();
+  switch(choice)
+  {
+    case '1': ictrainer();
+            break;
+    case '2': ictester();
+            break;
+    default: ictrainer();
+  }
 }
 void ictester()
 {}
